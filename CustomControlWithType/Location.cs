b581be582d22e352e36d6c3617da5160f22cf980
Tim Heuer <tim@timheuer.com>
2017-02-15 11:58:48 -0800

    Initial commit

---
+++ CustomControlWithType/Location.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CustomControlWithType
+{
+    [Windows.Foundation.Metadata.CreateFromString(MethodName = "CustomControlWithType.Location.ConvertToLatLong")]
+    public class Location
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Altitude { get; set; }
+
+        public static Location ConvertToLatLong(string rawString)
+        {
+            string[] coords = rawString.Split(',');
+            
+            var position = new Location();
+            position.Latitude = Convert.ToDouble(coords[0]);
+            position.Longitude = Convert.ToDouble(coords[1]);
+
+            if (coords.Length > 2)
+            {
+                position.Altitude = Convert.ToDouble(coords[2]);
+            }
+
+            return position;
+        }
+    }
+}
---
+++ CustomControlWithType/MyMap.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace CustomControlWithType
+{
+    public class MyMap : Control
+    {
+        public MyMap()
+        {
+            this.DefaultStyleKey = typeof(MyMap);
+        }
+
+        public string MapTitle { get; set; }
+        public Location CenterPoint { get; set; }
+    }
+}
